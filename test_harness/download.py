9968cfa8f06b3bc513f6848e935feaa1099f6e92
Max Wang <max@covar.com>
2024-01-26 10:17:42 -0500

    Use test suite instead of all the test cases

--- test_harness/download.py
+++ test_harness/download.py
@@ -6,7 +6,7 @@ import json
 import logging
 from pathlib import Path
 import tempfile
-from typing import List, Union
+from typing import List, Union, Dict
 import zipfile
 
 from translator_testing_model.datamodel.pydanticmodel import TestCase, TestSuite
@@ -14,7 +14,7 @@ from translator_testing_model.datamodel.pydanticmodel import TestCase, TestSuite
 
 def download_tests(
     suite: Union[str, List[str]], url: Path, logger: logging.Logger,
-) -> List[TestCase]:
+) -> Dict[str, TestCase]:
     """Download tests from specified location."""
     assert Path(url).suffix == ".zip"
     logger.info(f"Downloading tests from {url}...")
@@ -28,42 +28,45 @@ def download_tests(
             zip_ref.extractall(tmpdir)
 
         # Find all json files in the downloaded zip
-        tests_paths = glob.glob(f"{tmpdir}/**/*.json", recursive=True)
+        # tests_paths = glob.glob(f"{tmpdir}/**/*.json", recursive=True)
 
-        all_tests = []
-        suites = suite if type(suite) == list else [suite]
-        test_case_ids = []
+        tests_paths = glob.glob(f"{tmpdir}/*/test_suites/test_suite_output.json")
+        
+        with open(tests_paths[0]) as f:
+            test_suite = TestSuite.parse_obj(json.load(f))
+        
+        # all_tests = []
+        # suites = suite if type(suite) == list else [suite]
+        # test_case_ids = []
 
-        logger.info(f"Reading in {len(tests_paths)} tests...")
+        # logger.info(f"Reading in {len(test_suite.test_cases)} tests...")
 
         # do the reading of the tests and make a tests list
-        for test_path in tests_paths:
-            with open(test_path, "r") as f:
-                test_json = json.load(f)
-                try:
-                    # test_suite = TestSuite.parse_obj(test_json)
-                    # if test_suite.id in suites:
-                    # if test_json["test_case_type"] == "acceptance":
-                    #     # if suite is selected, grab all its test cases
-                    #     # test_case_ids.extend(test_suite.case_ids)
-                    #     all_tests.append(test_json)
-                    #     continue
-                    if test_json.get("test_env"):
-                        # only grab Test Cases and not Test Assets
-                        all_tests.append(test_json)
-                except Exception as e:
-                    # not a Test Suite
-                    pass
-                # try:
-                #     # test_case = TestCase.parse_obj(test_json)
-                #     if test_json["test_case_type"] == "quantitative":
-                #         all_tests.append(test_json)
-                #         continue
-                #     # all_tests.append(test_json)
-                # except Exception as e:
-                #     # not a Test Case
-                #     print(e)
-                #     pass
+        # for test_case in test_suite.test_cases:
+        #     try:
+                # test_suite = TestSuite.parse_obj(test_json)
+                # if test_suite.id in suites:
+                # if test_json["test_case_type"] == "acceptance":
+                #     # if suite is selected, grab all its test cases
+                #     # test_case_ids.extend(test_suite.case_ids)
+                #     all_tests.append(test_json)
+                #     continue
+            #     if test_json.get("test_env"):
+            #         # only grab Test Cases and not Test Assets
+            #         all_tests.append(test_json)
+            # except Exception as e:
+            #     # not a Test Suite
+            #     pass
+            # try:
+            #     # test_case = TestCase.parse_obj(test_json)
+            #     if test_json["test_case_type"] == "quantitative":
+            #         all_tests.append(test_json)
+            #         continue
+            #     # all_tests.append(test_json)
+            # except Exception as e:
+            #     # not a Test Case
+            #     print(e)
+            #     pass
 
     # only return the tests from the specified suites
     # tests = list(filter(lambda x: x in test_case_ids, all_tests))
@@ -75,7 +78,7 @@ def download_tests(
     # ]
     # for test in tests:
     #     test.test_case_type = "acceptance"
-    tests = all_tests
+    # tests = all_tests
     # tests = list(filter((lambda x: x for x in all_tests for asset in x.test_assets if asset.output_id), all_tests))
-    logger.info(f"Passing along {len(tests)} tests")
-    return tests
+    logger.info(f"Passing along {len(test_suite.test_cases)} tests")
+    return test_suite.test_cases
