7672c87673e7ff005080f62dffb6a7ea5579d16c
Max Wang <max@covar.com>
2024-01-26 10:37:44 -0500

    Fix and add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,29 @@
 import pytest
 
 from test_harness.main import main
-from .example_tests import example_tests
+from .example_tests import example_test_cases
+
+from .mocker import (
+    MockReporter,
+    MockSlacker,
+)
 
 
-def test_main(mocker):
+@pytest.mark.asyncio
+async def test_main(mocker):
     """Test the main function."""
     # This article is awesome: https://nedbatchelder.com/blog/201908/why_your_mock_doesnt_work.html
-    run_ui_test = mocker.patch("test_harness.run.run_ui_test", return_value="Pass")
     run_ars_test = mocker.patch("test_harness.run.run_ars_test", return_value="Fail")
-    main(
+    run_tests = mocker.patch("test_harness.main.run_tests", return_value={})
+    mocker.patch("test_harness.slacker.Slacker", return_value=MockSlacker())
+    mocker.patch("test_harness.main.Reporter", return_value=MockReporter())
+    await main(
         {
-            "tests": example_tests,
+            "tests": example_test_cases,
             "save_to_dashboard": False,
             "json_output": False,
             "log_level": "ERROR",
         }
     )
-    run_ui_test.assert_called_once()
-    run_ars_test.assert_called_once()
+    # run_ui_test.assert_called_once()
+    run_tests.assert_called_once()
---
+++ tests/test_run.py
@@ -0,0 +1,45 @@
+import pytest
+
+from test_harness.run import run_tests
+from .example_tests import example_test_cases
+
+from .mocker import (
+    MockReporter,
+    MockSlacker,
+)
+
+
+@pytest.mark.asyncio
+async def test_run_tests(mocker):
+    """Test the run_tests function."""
+    # This article is awesome: https://nedbatchelder.com/blog/201908/why_your_mock_doesnt_work.html
+    run_ars_test = mocker.patch("test_harness.run.run_ars_test", return_value={
+        "pks": {
+            "parent_pk": "123abc",
+            "merged_pk": "456def",
+        },
+        "results": [
+            {
+                "ars": {
+                    "status": "PASSED",
+                },
+            },
+            {
+                "ars": {
+                    "status": "FAILED",
+                    "message": "",
+                },
+            },
+        ],
+    })
+    run_benchmarks = mocker.patch("test_harness.run.run_benchmarks", return_value={})
+    await run_tests(
+        reporter=MockReporter(
+            base_url="http://test",
+        ),
+        slacker=MockSlacker(),
+        tests=example_test_cases,
+    )
+    # run_ui_test.assert_called_once()
+    run_ars_test.assert_called_once()
+    run_benchmarks.assert_not_called()
