9dd96d4d3e0e44dd8a779cb22a774ce52ac01f8f
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2024-05-15 17:09:40 -0700

    Extract code comments into supplemental README

--- test_harness/run.py
+++ test_harness/run.py
@@ -226,31 +226,7 @@ async def run_tests(
             # full_report[test["test_case_input_id"]]["ars"] = ars_result
 
         elif test.test_case_objective in ("StandardsValidationTest", "OneHopTest"):
-            # One Hop Tests and TRAPI/Biolink ("standards") validation test have comparable test inputs and
-            # configuration, except for the TestRunner that is executed, hence, are aggregated in this section.
-
-            # 1. Standards Validation TestRunner uses the "reasoner-validator" package to validate
-            # TRAPI and Biolink Model compliance of inputs and outputs templated TRAPI queries.
-
-            # 2. One Hop Tests seem a bit different from other types of tests. Generally, a single OneHopTest TestAsset
-            # is single S-P-O triplet with categories, used internally to generate a half dozen distinct TestCases and
-            # a single KP or ARA TRAPI service is called several times, once for each generated TestCase.
-            #
-            # There is no sense of "ExpectedOutput" in the tests rather, test pass, fail or skip status is an intrinsic
-            # outcome pertaining to the recovery of the input test asset values in the output of the various TestCases.
-            # A list of such TestAssets run against a given KP or ARA target service, could be deemed a "TestSuite".
-            # But a set of such TestSuites could be run in batch within a given TestSession. It is somewhat hard
-            # to align with this framework to the new Translator Test Harness, or at least, not as efficient to run.
-            #
-            # To make this work, we will do some violence to the testing model and wrap each input S-P-O triple as a
-            # single TestCase, extract a single associated TestAsset, which we'll feed in with the value of the
-            # TestCase 'components' field value, which will be taken as the 'infores' of the ARA or KP to be tested.
-            #
-            # Internally, we'll generate and run TRAPI queries of the actual TestCase instances against the 'infores'
-            # specified resources, then return the results, suitably indexed.  Alternately, if the specified target
-            # is the 'ars', then the returned results will be indexed by 'pks'(?)
-
-            # As indicated above, we only expect a single TestAsset
+            # We only expect a single TestAsset
             asset = test.test_assets[0]
 
             # Remapping fields semantically onto
@@ -269,10 +245,10 @@ async def run_tests(
                 "trapi_version": trapi_version,
                 "biolink_version": biolink_version,
                 "runner_settings": asset.test_runner_settings,
-                # TODO: in principle, additional (optional) keyword arguments could be given in
-                #       the test_inputs as a means to configure the BiolinkValidator class in reasoner-validator
-                #       with additional parameters like target_provenance and strict_validation; however,
-                #       it is unclear at this moment where and how these can or should be specified.
+                #
+                #  See README_Graph_Validation_Test_Runners.md
+                #  for explanation of possible additional **kwargs
+                #
                 # **kwargs
             }
             err_msg = ""
