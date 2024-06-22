18dc5cce9bd2a48a4af388f81f2680a8644b1934
Max Wang <max@covar.com>
2024-06-22 08:27:56 -0400

    Take out that break

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as readme_file:
 
 setup(
     name="sri-test-harness",
-    version="0.2.1",
+    version="0.2.2",
     author="Max Wang",
     author_email="max@covar.com",
     url="https://github.com/TranslatorSRI/TestHarness",
